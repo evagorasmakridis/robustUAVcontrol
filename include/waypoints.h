658f745467993c1bd83e0af9a489536cdc077eb3
Evagoras Makridis <evagoras.makridis@gmail.com>
2019-06-21 11:40:34 +0300

    Added - Waypoints mission

---
+++ include/waypoints.h
@@ -0,0 +1,261 @@
+/** @file demo_local_position_control.h
+ *  @version 3.3
+ *  @date September, 2017
+ *
+ *  @brief
+ *  demo sample of how to use local position control APIs
+ *
+ *  @copyright 2017 DJI. All rights reserved.
+ *
+ */
+
+#ifndef PROJECT_DEMO_LOCAL_POSITION_CONTROL_H
+#define PROJECT_DEMO_LOCAL_POSITION_CONTROL_H
+
+#endif //PROJECT_DEMO_LOCAL_POSITION_CONTROL_H
+#include <dji_sdk/SetLocalPosRef.h>
+#include <ros/ros.h>
+#include <dji_sdk_demo/Pos.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/QuaternionStamped.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <std_msgs/UInt8.h>
+#include <sensor_msgs/Joy.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <geometry_msgs/Vector3.h>
+//DJI SDK includes
+#include <dji_sdk/DroneTaskControl.h>
+#include <dji_sdk/SDKControlAuthority.h>
+#include <dji_sdk/QueryDroneVersion.h>
+#include <tf/tf.h>
+
+#include <eigen3/Eigen/Eigen>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Eigenvalues>
+#include <assert.h>
+#include <complex>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#define C_EARTH (double)6378137.0
+#define C_PI (double)3.141592653589793
+#define DEG2RAD(DEG) ((DEG) * ((C_PI) / (180.0)))
+#define POS_THRESH 0.3
+
+sensor_msgs::NavSatFix current_gps_position;
+sensor_msgs::Imu curr_imu;
+geometry_msgs::PointStamped local_position;
+geometry_msgs::Point initial_pos;
+geometry_msgs::PointStamped uwb_pos;
+geometry_msgs::Point prev_pos;
+geometry_msgs::Quaternion current_atti;
+geometry_msgs::Vector3 rpy;
+dji_sdk_demo::Pos curr_pos;
+
+bool set_local_position();
+
+int target_set_state = 0;
+
+int k = 0; // Sampling step
+int n = 8; // Number of states
+int m = 4; // Number of measurements
+int j = 2; // Number of inputs
+int T = 2; // Sliding window size (samples)
+int sigma = 100; // kernel size mcckf
+
+float target_x;
+float target_y;
+float target_z;
+float target_psi;
+
+float x_error=0;
+float y_error=0;
+
+// control inputs
+float roll_cmd = 0;
+float pitch_cmd = 0;
+float yaw_cmd = 0;
+float z_cmd = 0;
+
+double dt,dt1;
+ros::Time start_, end_,s1,e1;
+
+Eigen::MatrixXf A(n, n); // System dynamics matrix
+Eigen::MatrixXf B(n, j); // Control input matrix
+Eigen::MatrixXf C(m, n); // Output matrix
+Eigen::MatrixXf Cc(j, n); // Control output matrix
+//Eigen::MatrixXd Q(n, n); // State penalize matrix
+//Eigen::MatrixXd R(j, j); // Input penalize matrix
+Eigen::MatrixXf Qn(n, n); // Process noise covariance
+Eigen::MatrixXf Rn(m, m); // Measurement noise covariance
+Eigen::MatrixXf P0(n, n); // Estimate error covariance
+Eigen::MatrixXf SW(m,T); // Sliding window measurement noise error covariance
+Eigen::MatrixXf In(n, n); // Identity matrix
+Eigen::MatrixXf Im(m, m); // Identity matrix
+Eigen::MatrixXf L(j, n); // LQR control gain matrix
+Eigen::MatrixXf Lc(j, j); // Gain filter tracking matrix
+Eigen::VectorXf x0(n); // Initial states
+Eigen::VectorXf u(j); // Control input vector
+Eigen::VectorXf servo_compensator(j); // Lc(G)*ref
+Eigen::VectorXf y(m); // Measurement vector
+Eigen::VectorXf ref(j); // Target reference vector
+
+void lqg();
+Eigen::MatrixXf sliding_window(Eigen::VectorXf y);
+Eigen::VectorXf setTarget(float x, float y, float z, float psi);
+
+void imu_callback(const sensor_msgs::Imu::ConstPtr& msg);
+
+void initial_pos_callback(const geometry_msgs::Point::ConstPtr& msg);
+
+void uwb_position_callback(const dji_sdk_demo::Pos::ConstPtr& msg);
+
+void local_position_callback(const geometry_msgs::PointStamped::ConstPtr& msg);
+
+void display_mode_callback(const std_msgs::UInt8::ConstPtr& msg);
+
+void attitude_callback(const geometry_msgs::QuaternionStamped::ConstPtr& msg);
+
+void flight_status_callback(const std_msgs::UInt8::ConstPtr& msg);
+
+void gps_position_callback(const sensor_msgs::NavSatFix::ConstPtr& msg);
+
+void gps_health_callback(const std_msgs::UInt8::ConstPtr& msg);
+
+bool takeoff_land(int task);
+
+bool obtain_control();
+
+geometry_msgs::Vector3 toEulerAngle(geometry_msgs::Quaternion quat);
+
+bool is_M100();
+
+bool monitoredTakeoff();
+
+bool M100monitoredTakeoff();
+
+void local_position_ctrl();
+
+class KalmanFilter {
+	public:
+		/* Problem Dimension */
+		int n; //State vector dimension
+		int m; //Control vector (input) dimension (if there is not input, set to zero)
+		/* Fixed Matrix */
+		Eigen::MatrixXf A; //System dynamics matrix
+		Eigen::MatrixXf B; //Control matrix 
+		Eigen::MatrixXf C; //Mesaurement Adaptation matrix
+		Eigen::MatrixXf Q; //Process Noise Covariance matrix
+		Eigen::MatrixXf R; //Measurement Noise Covariance matrix
+		Eigen::MatrixXf I; //Identity matrix
+		/* Variable Matrix */
+		Eigen::VectorXf X; //(Current) State vector
+		Eigen::MatrixXf P; //State Covariance
+		Eigen::MatrixXf K; //Kalman Gain matrix
+		/* Inizial Value */
+		Eigen::VectorXf X0; //Initial State vector
+		Eigen::MatrixXf P0; //Initial State Covariance matrix
+		/* 
+		* Constructor 
+		* _n: state vector dimension
+		* _m: control vector dimension (if there is not input, set to zero)
+		*/
+		KalmanFilter(int _n,  int _m);
+		/* Set Fixed Matrix (NO INPUT) */
+		void setFixed ( Eigen::MatrixXf _A, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R );
+		/* Set Fixed Matrix (WITH INPUT) */
+		void setFixed ( Eigen::MatrixXf _A, Eigen::MatrixXf _B, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R );
+		/* Set Initial Value */
+		void setInitial( Eigen::VectorXf _X0, Eigen::MatrixXf _P0 );
+		/* Do prediction (NO INPUT) */
+		void predict ( void );
+		/* Do prediction (INPUT) */
+		void predict ( Eigen::VectorXf U );
+		/* Do correction */
+		void correct ( Eigen::VectorXf Y); // add Eigen::MatrixXf _R for dynamic R matrix
+};
+
+class MCCKalmanFilter {
+	public:
+		/* Problem Dimension */
+		int n; //State vector dimension
+		int m; //Control vector (input) dimension (if there is not input, set to zero)
+		int sigma;
+		/* Fixed Matrix */
+		Eigen::MatrixXf A; //System dynamics matrix
+		Eigen::MatrixXf B; //Control matrix 
+		Eigen::MatrixXf C; //Mesaurement Adaptation matrix
+		Eigen::MatrixXf Q; //Process Noise Covariance matrix
+		Eigen::MatrixXf R; //Measurement Noise Covariance matrix
+		Eigen::MatrixXf I; //Identity matrix
+		/* Variable Matrix */
+		Eigen::VectorXf X; //(Current) State vector
+		Eigen::MatrixXf P; //State Covariance
+		Eigen::MatrixXf K; //Kalman Gain matrix
+		/* Inizial Value */
+		Eigen::VectorXf X0; //Initial State vector
+		Eigen::MatrixXf P0; //Initial State Covariance matrix
+		/* 
+		* Constructor 
+		* _n: state vector dimension
+		* _m: control vector dimension (if there is not input, set to zero)
+		*/
+		MCCKalmanFilter(int _n,  int _m, int _sigma);
+		/* Set Fixed Matrix (NO INPUT) */
+		void setFixed ( Eigen::MatrixXf _A, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R );
+		/* Set Fixed Matrix (WITH INPUT) */
+		void setFixed ( Eigen::MatrixXf _A, Eigen::MatrixXf _B, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R );
+		/* Set Initial Value */
+		void setInitial( Eigen::VectorXf _X0, Eigen::MatrixXf _P0 );
+		/* Do prediction (NO INPUT) */
+		void predict ( void );
+		/* Do prediction (INPUT) */
+		void predict ( Eigen::VectorXf U);
+		/* Do correction */
+		void correct ( Eigen::VectorXf Y );
+
+};
+
+class HinfFilter {
+	public:
+		/* Problem Dimension */
+		int n; //State vector dimension
+		int m; //Control vector (input) dimension (if there is not input, set to zero)
+		int theta;
+		/* Fixed Matrix */
+		Eigen::MatrixXf A; //System dynamics matrix
+		Eigen::MatrixXf B; //Control matrix 
+		Eigen::MatrixXf C; //Mesaurement Adaptation matrix
+		Eigen::MatrixXf Q; //Process Noise Covariance matrix
+		Eigen::MatrixXf R; //Measurement Noise Covariance matrix
+		Eigen::MatrixXf I; //Identity matrix
+		/* Variable Matrix */
+		Eigen::VectorXf X; //(Current) State vector
+		Eigen::MatrixXf P; //State Covariance
+		Eigen::MatrixXf K; //Kalman Gain matrix
+		/* Inizial Value */
+		Eigen::VectorXf X0; //Initial State vector
+		Eigen::MatrixXf P0; //Initial State Covariance matrix
+		/* 
+		* Constructor 
+		* _n: state vector dimension
+		* _m: control vector dimension (if there is not input, set to zero)
+		*/
+		HinfFilter(int _n,  int _m, int _sigma);
+		/* Set Fixed Matrix (NO INPUT) */
+		void setFixed ( Eigen::MatrixXf _A, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R );
+		/* Set Fixed Matrix (WITH INPUT) */
+		void setFixed ( Eigen::MatrixXf _A, Eigen::MatrixXf _B, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R );
+		/* Set Initial Value */
+		void setInitial( Eigen::VectorXf _X0, Eigen::MatrixXf _P0 );
+		/* Do prediction (NO INPUT) */
+		void predict ( void );
+		/* Do prediction (INPUT) */
+		void predict ( Eigen::VectorXf U );
+		/* Do correction */
+		void correct ( Eigen::VectorXf Y );
+};
+
---
+++ src/waypoints.cpp
@@ -0,0 +1,540 @@
+/** @file demo_flight_control.cpp
+*  @version 3.3
+*  @date September, 2017
+*
+*  @brief
+*  demo sample of how to use Local position control
+*
+*  @copyright 2017 DJI. All rights reserved.
+*
+*/
+
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include "dji_sdk_demo/hover.h"
+#include "dji_sdk/dji_sdk.h"
+
+ros::ServiceClient set_local_pos_reference;
+ros::ServiceClient sdk_ctrl_authority_service;
+ros::ServiceClient drone_task_service;
+ros::ServiceClient query_version_service;
+//ros::Publisher ctrlPosYawPub;
+ros::Publisher ctrlRollPitchYawHeightPub;
+
+// global variables for subscribed topics
+uint8_t flight_status = 255;
+uint8_t display_mode = 255;
+uint8_t current_gps_health = 0;
+
+float kp = 0.1; // proportional gain P
+float ki = 0.005; // integral gain I
+float kd = 4.5; // derivative gain D
+float max_angle = 0.1; // in rads
+
+// log files
+std::ofstream position_file;
+std::ofstream cct_file;
+std::ofstream variance_file;
+std::ofstream orientation_file;
+std::ofstream states_file;
+std::ofstream controls_file;
+std::ofstream gains_file;
+
+KalmanFilter estimator(n, m);
+//MCCKalmanFilter estimator(n, m, sigma);
+// HinfFilter kf(n, m);
+
+int main(int argc, char **argv){
+  // Discrete LTI 100Hz (imported from Matlab model)
+  u << 0, 0; // initial input
+  
+  A << 0.992, 0.019, 0, 0, 0, 0, 0, 0,
+       -0.745, 0.864, 0, 0, 0, 0, 0, 0,
+       0, 0, 0.993, 0.019, 0, 0, 0, 0,
+       0, 0, -0.715, 0.871, 0, 0, 0, 0,
+       0, 0, 0.002, 0, 1, 0.02, 0, 0,
+       0, 0, 0.196, 0.002, 0, 1.007, 0, 0,
+       -0.002, -0, 0, 0, 0, 0, 1, 0.02,
+       -0.196, -0.002, 0, 0, 0, 0, 0, 1.007;
+
+  B << 0.007, 0,
+       0.702, 0,
+       0, 0.007,
+       0, 0.676,
+       0, 0,
+       0, 0,
+       0, 0,
+       0, 0;
+
+  C << 1, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 1, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 1, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 1, 0;
+
+  Cc << 0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0;
+
+//  L << 0.2770, 0.0380, 0, 0, 0, 0, -0.0310, -0.1460,
+//       0, 0, 0.2770, 0.0390, 0.0310, 0.1450, 0, 0;
+  L << 0.746,0.091,0,0,-0,0,-0.097,-0.255,
+       0,0,0.754,0.096,0.097,0.255,-0,-0;
+
+  // LQR weighting matrices
+  // Q.setIdentity(n, n);
+  // R.setIdentity(j, j);
+  
+  // Kalman filter initial covariance matrices
+  In.setIdentity(n,n);
+  Im.setIdentity(m,m);
+  P0=4*In;
+  Qn=0.1*In;
+  Rn=1*Im;
+  Qn(0,0) = 0.001;
+  Qn(2,2) = 0.001;
+  Qn(4,4) = 1;
+  Qn(6,6) = 0.1;
+  Rn(0,0) = 0.003;
+  Rn(1,1) = 0.002;
+  Rn(2,2) = 2.4;
+  Rn(3,3) = 0.5;
+  
+  // Reference tracking
+  Lc = -(Cc*(In-A-B*L).inverse()*B).inverse();
+
+  ros::init(argc, argv, "demo_local_position_control_node");
+  ros::NodeHandle nh;
+  
+  // Subscribe to messages from dji_sdk_node
+  ros::Subscriber attitudeSub = nh.subscribe("dji_sdk/attitude", 10, &attitude_callback);
+  ros::Subscriber flightStatusSub = nh.subscribe("dji_sdk/flight_status", 10, &flight_status_callback);
+  //ros::Subscriber displayModeSub = nh.subscribe("dji_sdk/display_mode", 10, &display_mode_callback);
+  //ros::Subscriber localPosition = nh.subscribe("dji_sdk/local_position", 10, &local_position_callback);
+  //ros::Subscriber gpsSub = nh.subscribe("dji_sdk/gps_position", 10, &gps_position_callback);
+  //ros::Subscriber gpsHealth = nh.subscribe("dji_sdk/gps_health", 10, &gps_health_callback);
+  //ros::Subscriber initialPosition = nh.subscribe("initial_pos", 1, &initial_pos_callback);
+
+  // Subscribe to the sensor measurements
+  ros::Subscriber imu = nh.subscribe("/dji_sdk/imu", 2, &imu_callback);
+  ros::Subscriber uwbPosition = nh.subscribe("/uwb_pos", 1, &uwb_position_callback);
+
+  // Publish the control signal
+  //ctrlRollPitchYawHeightPub = nh.advertise<sensor_msgs::Joy>("dji_sdk/flight_control_setpoint_rollpitch_yawrate_zposition", 1);
+  //ctrlRollPitchYawHeightPub = nh.advertise<sensor_msgs::Joy>("dji_sdk/flight_control_setpoint_ENUposition_yaw", 10);
+  ctrlRollPitchYawHeightPub = nh.advertise<sensor_msgs::Joy>("dji_sdk/flight_control_setpoint_generic", 1);
+
+  // Basic services
+  sdk_ctrl_authority_service = nh.serviceClient<dji_sdk::SDKControlAuthority>("dji_sdk/sdk_control_authority");
+  drone_task_service = nh.serviceClient<dji_sdk::DroneTaskControl>("dji_sdk/drone_task_control");
+  query_version_service = nh.serviceClient<dji_sdk::QueryDroneVersion>("dji_sdk/query_drone_version");
+  set_local_pos_reference = nh.serviceClient<dji_sdk::SetLocalPosRef>("dji_sdk/set_local_pos_ref");
+
+  bool obtain_control_result = obtain_control();
+  bool takeoff_result;
+
+  position_file.open("/home/ubuntu/results/position.txt");
+  variance_file.open("/home/ubuntu/results/variance.txt");
+  orientation_file.open("/home/ubuntu/results/orientation.txt");
+  states_file.open("/home/ubuntu/results/states.txt");
+  controls_file.open("/home/ubuntu/results/controls.txt");
+  gains_file.open("/home/ubuntu/results/gains.txt");
+  cct_file.open("/home/ubuntu/results/control_computation_time.txt");
+
+  ROS_INFO("M100 taking off!");
+  takeoff_result = M100monitoredTakeoff();
+  if (takeoff_result){
+    printf("READY TO CONTROL \n");
+    target_set_state = 1;
+  }
+  ros::spin();
+  return 0;
+}
+
+geometry_msgs::Vector3 toEulerAngle(geometry_msgs::Quaternion quat){
+  geometry_msgs::Vector3 ans;
+
+  tf::Matrix3x3 R_FLU2ENU(tf::Quaternion(quat.x, quat.y, quat.z, quat.w));
+  R_FLU2ENU.getRPY(ans.x, ans.y, ans.z);
+  return ans;
+}
+
+void lqg(){
+  printf("\n-----------------------------------\nLQG control - (sample k = %d)\n-----------------------------------\n", k);
+  y << rpy.x, rpy.y, curr_pos.x, curr_pos.y;
+  servo_compensator = Lc*ref;
+
+  if (k==0){
+    // Initial states
+    x0 << rpy.x, 0, rpy.y, 0, curr_pos.x, 0, curr_pos.y, 0; // phi,phid,theta,thetad,psi,psid,z,zd,x,xd,y,yd
+    // Kalman filter initialization
+    estimator.setFixed(A, B, C, Qn, Rn);
+    estimator.setInitial(x0, P0);
+    // Initial control input
+    u << servo_compensator - L*x0;
+  }
+  else {
+    // LQG reference tracking control
+    u << servo_compensator - L*estimator.X; 
+  }
+
+  // Rn = sliding_window(y);
+  
+  // State estimation
+  estimator.predict(u);
+  estimator.correct(y);
+ 
+  // Bound angles and height for safety and linerized operating point
+  if (fabs(u[0]) >= max_angle)
+    roll_cmd = (u[0] > 0) ? max_angle : -1 * max_angle;
+  else
+    roll_cmd = u[0];
+  
+  if (fabs(u[1]) >= max_angle)
+    pitch_cmd = (u[1] > 0) ? max_angle : -1 * max_angle;
+  else
+    pitch_cmd = u[1];
+
+  // Publish controls to the drone
+  uint8_t flag = (DJISDK::VERTICAL_POSITION   |
+                DJISDK::HORIZONTAL_ANGLE |
+                DJISDK::YAW_RATE            |
+                DJISDK::HORIZONTAL_BODY  |
+                DJISDK::STABLE_ENABLE);
+  sensor_msgs::Joy controlmsg;
+  controlmsg.axes.push_back(roll_cmd); // (desired roll - movement along y-axis)
+  controlmsg.axes.push_back(pitch_cmd); // (desired pitch - movements along x-axis)
+  controlmsg.axes.push_back(1); // z-cmd (desired height in meters)
+  controlmsg.axes.push_back(0); // (desired yaw rate)
+  controlmsg.axes.push_back(flag);
+  ctrlRollPitchYawHeightPub.publish(controlmsg);
+
+  orientation_file << rpy.x << "," << rpy.y << "," << rpy.z << std::endl;
+  position_file << curr_pos.x << "," << curr_pos.y << "," << curr_pos.z << std::endl;
+  //variance_file << Rn(0,0) << "," << Rn(1,1) << "," << Rn(2,2) << "," << Rn(3,3) << std::endl;
+  states_file << estimator.X[0] << "," << estimator.X[1] << "," << estimator.X[2] << "," << estimator.X[3] << "," << estimator.X[4] << "," << estimator.X[5] << "," << estimator.X[6] << "," << estimator.X[7] << std::endl;
+  controls_file << roll_cmd << "," << pitch_cmd << std::endl;
+
+  x_error = ref[0] - estimator.X[4] 
+  y_error = ref[1] - estimator.X[6] 
+
+  k += 1; // sampling step
+}
+
+Eigen::MatrixXf sliding_window(Eigen::VectorXf y){
+    // Measurement noise sliding window covariance matrix
+    SW.row(T-1) = y;
+    std::cout << "Matrix :\n" << SW.transpose() << std::endl;
+    if (k<T){
+        Eigen::MatrixXf centered = SW.topRows(k).rowwise() - SW.topRows(k).colwise().mean();
+        Rn = (centered.adjoint() * centered) / double(SW.rows() - 1);
+    }
+    else{
+        Eigen::MatrixXf centered = SW.rowwise() - SW.colwise().mean();
+        Rn = (centered.adjoint() * centered) / double(SW.rows() - 1);
+    }
+    std::cout << "\nMeasurement online covariance matrix (Rn):\n" << Rn << "\n" << std::endl;
+    SW.topRows(T-1) = SW.bottomRows(T-1); 
+    return Rn;
+}
+
+Eigen::VectorXf setTarget(float x, float y, float z, float psi){
+  target_x = x;
+  target_y = y;
+  target_z = z;
+  target_psi = psi;
+  
+  ref << target_x, target_y, target_z, target_psi
+  
+  return ref;
+}
+
+void uwb_position_callback(const dji_sdk_demo::Pos::ConstPtr &msg){
+  curr_pos = *msg;
+  
+  if (target_set_state >= 1) {
+    lqg(); //pid_pos_form();
+  }
+
+  switch(target_set_state){
+    case 1:
+      ref << setTarget(3.0,2.0,1.0,0.0); // x,y,z,psi
+      if((fabs(x_error)<POS_THRESH)&&(fabs(y_error)<POS_THRESH)){
+          printf("Going to Waypoint 2 (x:5, y:2)\n" );
+          target_set_state=1;
+      }
+      break;
+    case 2:
+      ref << setTarget(5.0,2.0,1.0,0.0); // x,y,z,psi
+      if((fabs(x_error)<POS_THRESH)&&(fabs(y_error)<POS_THRESH)){
+          printf("Going to Waypoint 3 (x:5, y:3)\n" );
+          target_set_state=2;
+      }     
+      break;
+    case 3:
+      ref << setTarget(5.0,3.0,1.0,0.0); // x,y,z,psi
+      if((fabs(x_error)<POS_THRESH)&&(fabs(y_error)<POS_THRESH)){
+          printf("Going to Waypoint 4 (x:3, y:3)\n" );
+          target_set_state=3;
+      }
+      break;
+    case 4:
+      ref << setTarget(3.0,3.0,1.0,0.0); // x,y,z,psi
+      if((fabs(x_error)<POS_THRESH)&&(fabs(y_error)<POS_THRESH)){
+          printf("Going to Waypoint 1 (x:3, y:2)\n" );
+          target_set_state=1;
+      }     
+      break;
+  }
+}
+
+void imu_callback(const sensor_msgs::Imu::ConstPtr &msg){
+  curr_imu = *msg;
+}
+
+void local_position_callback(const geometry_msgs::PointStamped::ConstPtr &msg){
+  local_position = *msg;
+}
+
+void flight_status_callback(const std_msgs::UInt8::ConstPtr &msg){
+  flight_status = msg->data;
+}
+
+void display_mode_callback(const std_msgs::UInt8::ConstPtr &msg){
+  display_mode = msg->data;
+}
+void attitude_callback(const geometry_msgs::QuaternionStamped::ConstPtr &msg){
+  current_atti = msg->quaternion;
+  rpy = toEulerAngle(current_atti);
+}
+
+void gps_position_callback(const sensor_msgs::NavSatFix::ConstPtr &msg){
+  current_gps_position = *msg;
+}
+
+void gps_health_callback(const std_msgs::UInt8::ConstPtr &msg){
+  current_gps_health = msg->data;
+}
+
+bool takeoff_land(int task){
+  dji_sdk::DroneTaskControl droneTaskControl;
+
+  droneTaskControl.request.task = task;
+
+  drone_task_service.call(droneTaskControl);
+
+  if (!droneTaskControl.response.result){
+    ROS_ERROR("takeoff_land fail");
+    return false;
+  }
+
+  return true;
+}
+
+bool obtain_control(){
+  dji_sdk::SDKControlAuthority authority;
+  authority.request.control_enable = 1;
+  sdk_ctrl_authority_service.call(authority);
+
+  if (!authority.response.result){
+    ROS_ERROR("obtain control failed!");
+    return false;
+  }
+
+  return true;
+}
+
+bool is_M100(){
+  dji_sdk::QueryDroneVersion query;
+  query_version_service.call(query);
+
+  if (query.response.version == DJISDK::DroneFirmwareVersion::M100_31){
+    return true;
+  }
+
+  return false;
+}
+
+/*!
+ * This function demos how to use the flight_status
+ * and the more detailed display_mode (only for A3/N3)
+ * to monitor the take off process with some error
+ * handling. Note M100 flight status is different
+ * from A3/N3 flight status.
+ */
+
+bool M100monitoredTakeoff(){
+  ros::Time start_time = ros::Time::now();
+
+  float home_altitude = current_gps_position.altitude;
+  if (!takeoff_land(dji_sdk::DroneTaskControl::Request::TASK_TAKEOFF)){
+    return false;
+  }
+
+  ros::Duration(0.01).sleep();
+  ros::spinOnce();
+
+  // Step 1: If M100 is not in the air after 10 seconds, fail.
+  // Lets do it after 4s
+  while (ros::Time::now() - start_time < ros::Duration(4)){
+    ros::Duration(0.01).sleep();
+    ros::spinOnce();
+  }
+
+  if (flight_status != DJISDK::M100FlightStatus::M100_STATUS_IN_AIR){
+    ROS_INFO("Takeoff failed.");
+    return false;
+  }
+  else{
+    start_time = ros::Time::now();
+    ROS_INFO("Successful takeoff!");
+    ros::spinOnce();
+  }
+  return true;
+}
+
+using namespace std;
+// Kalman filter constructor:
+KalmanFilter::KalmanFilter(int _n,  int _m) {
+	n = _n;
+	m = _m;
+}
+// Set Fixed Matrix
+void KalmanFilter::setFixed( Eigen::MatrixXf _A, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R ){
+	A = _A;
+	C = _C;
+	Q = _Q;
+	R = _R;
+	I = I.Identity(n, n);
+}
+// Set Fixed Matrix
+void KalmanFilter::setFixed( Eigen::MatrixXf _A, Eigen::MatrixXf _B, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R){
+	A = _A;
+	B = _B;
+	C = _C;
+	Q = _Q;
+	R = _R;
+	I = I.Identity(n, n);
+}
+// Set Initial Matrix
+void KalmanFilter::setInitial( Eigen::VectorXf _X0, Eigen::MatrixXf _P0 ){
+	X0 = _X0;
+	P0 = _P0;
+}
+// Do prediction based of physical system (No external input)
+void KalmanFilter::predict(void){
+  X = (A * X0);
+  P = (A * P0 * A.transpose()) + Q;
+}
+// Do prediction based of physical system (with external input)  U: Control vector
+void KalmanFilter::predict( Eigen::VectorXf U ){
+  X = (A * X0) + (B * U);
+  P = (A * P0 * A.transpose()) + Q;
+}
+// Correct the prediction, using mesaurement Y: mesaure vector
+void KalmanFilter::correct ( Eigen::VectorXf Y) {
+  K = ( P * C.transpose() ) * ( C * P * C.transpose() + R).inverse();
+  X = X + K*(Y - C * X);
+  P = (I - K * C) * P;
+  X0 = X;
+  P0 = P;
+}
+
+// MCC Kalman filter constructor:
+MCCKalmanFilter::MCCKalmanFilter(int _n,  int _m, int _sigma) {
+	n = _n;
+	m = _m;
+  sigma = _sigma;
+}
+// Set Fixed Matrix
+void MCCKalmanFilter::setFixed( Eigen::MatrixXf _A, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R ){
+	A = _A;
+	C = _C;
+	Q = _Q;
+	R = _R;
+	I = I.Identity(n, n);
+}
+// Set Fixed Matrix
+void MCCKalmanFilter::setFixed( Eigen::MatrixXf _A, Eigen::MatrixXf _B, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R){
+	A = _A;
+	B = _B;
+	C = _C;
+	Q = _Q;
+	R = _R;
+	I = I.Identity(n, n);
+}
+// Set Initial Matrix
+void MCCKalmanFilter::setInitial( Eigen::VectorXf _X0, Eigen::MatrixXf _P0 ){
+	X0 = _X0;
+	P0 = _P0;
+}
+// Do prediction based of physical system (No external input)	 
+void MCCKalmanFilter::predict(void){
+  X = (A * X0);
+  P = (A * P0 * A.transpose()) + Q;
+}
+// Do prediction based of physical system (with external input) U: Control vector	 
+void MCCKalmanFilter::predict( Eigen::VectorXf U ){
+  X = (A * X0) + (B * U);
+  P = (A * P0 * A.transpose()) + Q;
+}
+// Correct the prediction, using mesaurement  Y: mesaure vector
+void MCCKalmanFilter::correct ( Eigen::VectorXf Y ) {
+  // norm_innov = sqrt((innov).'*invers_R*(innov));
+  // Gkernel = exp(-(pow(norm(Y - C*X),2)) /(2*pow(sigma,2)))/exp(-(pow(norm(X - A*X),2))/(2*pow(sigma,2)));
+  Eigen::VectorXf innov(m);
+  Eigen::VectorXf innovx(n);
+  innov = Y-C*X;
+  innovx = X-A*X;
+  K = (P.inverse() + exp(-(pow(innov.norm(),2)) /(2*pow(sigma,2)))/exp(-(pow(innovx.norm(),2))/(2*pow(sigma,2))) * C.transpose()*R.inverse()*C).inverse() * exp(-(pow(innov.norm(),2)) /(2*pow(sigma,2)))/exp(-(pow(innovx.norm(),2))/(2*pow(sigma,2))) * C.transpose()*R.inverse();  X = X + K *(Y - C*X);
+  P = (I - K*C) * P * (I - K*C).transpose() + (K*R*K.transpose());
+  X0 = X;
+  P0 = P;
+}
+
+// H infinity filter constructor:
+HinfFilter::HinfFilter(int _n,  int _m, int _theta) {
+	n = _n;
+	m = _m;
+  theta = _theta;
+}
+// Set Fixed Matrix
+void HinfFilter::setFixed( Eigen::MatrixXf _A, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R ){
+	A = _A;
+	C = _C;
+	Q = _Q;
+	R = _R;
+	I = I.Identity(n, n);
+}
+// Set Fixed Matrix
+void HinfFilter::setFixed( Eigen::MatrixXf _A, Eigen::MatrixXf _B, Eigen::MatrixXf _C, Eigen::MatrixXf _Q, Eigen::MatrixXf _R){
+	A = _A;
+	B = _B;
+	C = _C;
+	Q = _Q;
+	R = _R;
+	I = I.Identity(n, n);
+}
+// Set Initial Matrix 
+void HinfFilter::setInitial( Eigen::VectorXf _X0, Eigen::MatrixXf _P0 ){
+	X0 = _X0;
+	P0 = _P0;
+}
+// Do prediction based of physical system (No external input)	 
+void HinfFilter::predict(void){
+  X = (A * X0);
+  P = (A * P0 * A.transpose()) + Q;
+}
+// Do prediction based of physical system (with external input)  U: Control vector	 
+void HinfFilter::predict( Eigen::VectorXf U ){
+  X = (A * X0) + (B * U);
+  P = (A * P0 * A.transpose()) + Q;
+}
+// Correct the prediction, using mesaurement  Y: mesaure vector
+void HinfFilter::correct ( Eigen::VectorXf Y ) {
+  K = P * (I-theta*P+C.transpose()*R.inverse()*C*P).inverse()*C.transpose()*R.inverse();
+  X = X + K * (Y - C*X);
+  P = P * (I - theta*P + C.transpose() * R.inverse() * C * P).inverse();
+  X0 = X;
+  P0 = P;
+}
